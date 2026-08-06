1cd1addcb6d950266e4e5bd669aa42db8871240f
lnhuyen2908 <lnhuyen2908@gmail.com>
2026-08-06 08:51:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/GiaiDoan3/",
+  base: "QLBTThucTap/GiaiDoan3/",
   plugins: [react(), tailwindcss()],
 });
